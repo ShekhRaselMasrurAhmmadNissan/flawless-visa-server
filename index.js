d2870357a78e191800bb54792879856c16fd97ea
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-09 14:58:51 +0600

    Connect the Mongodb Server.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -8,9 +9,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-	res.send('Flawless Visa server is running.');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@flawless-visa.92a8cfb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
 });
+console.log(uri);
+
+const run = async () => {
+	// const ServicesCollection = client.db('Flawless-Visa').collect;
+
+	app.get('/', async (req, res) => {
+		res.send('Flawless Visa server is running.');
+	});
+};
+
+run().catch((error) => console.error(error.name, error.message, error.stack));
 
 app.listen(port, () => {
 	console.log(`Flawless Visa Server is Running at port: ${port}`);
